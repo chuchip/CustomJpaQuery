78341958533ac5f8a3cce71e733f7ceb688a2b89
chuchip <chuchip@gmail.com>
2019-07-03 13:39:17 +0200

    Añadido ejemplo de query montada a pelo

--- src/main/java/com/profesorp/customjpaquery/controller/CustomController.java
+++ src/main/java/com/profesorp/customjpaquery/controller/CustomController.java
@@ -4,6 +4,9 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,9 @@ import com.profesorp.customjpaquery.repositories.CustomersRepository;
 public class CustomController {
 	@Autowired
 	CustomersRepository customersRepository;
+	
+	@Autowired
+	EntityManager em;
 	
 	public static final String GREATER_THAN="greater";
 	public static final String LESS_THAN="less";
@@ -47,6 +53,54 @@ public class CustomController {
 		}
 		
 		return customersRepository.getData(data);		
+		
+	}
+	@GetMapping("/getQuery")
+	public List<CustomersEntity> getDataQuery(@RequestParam(required=false,name="idCustomer") Integer idCustomer,
+			@RequestParam(value="nameCustomer",required=false) String nameCustomer,
+			@RequestParam(required=false) String addressCustomer, 
+			@RequestParam(required=false) @DateTimeFormat(pattern="dd-MM-yyyy") Date createdDate,@RequestParam(required=false) String dateCondition			)
+	{		
+		HashMap<String, Object> data=new HashMap<>();
+		
+		
+		String sql="select e from CustomersEntity e where 1=1";
+		
+		if (idCustomer!=null)
+			sql+=" and e.id = :idCustomer";
+		if (nameCustomer!=null)
+			sql+=" and e.name = :nameCustomer";
+			
+		if (addressCustomer!=null)
+			sql+=" and e.address = :addressCustomer";
+		String cond;
+		if (dateCondition==null)
+			dateCondition=GREATER_THAN;		 
+		switch (dateCondition)
+		{
+			case GREATER_THAN:
+				cond=">";
+				break;
+			case LESS_THAN:
+				cond="<";
+				break;
+			default:
+				cond="=";				
+		}	
+		
+		if (createdDate!=null)
+			sql+=" and e.created "+cond+" :createdDate";
+		TypedQuery<CustomersEntity> query= em.createQuery(sql,CustomersEntity.class);
+		if (idCustomer!=null)
+			query.setParameter("idCustomer", idCustomer);
+		if (nameCustomer!=null)
+			query.setParameter("nameCustomer", nameCustomer);
+			
+		if (addressCustomer!=null)
+			query.setParameter("addressCustomer", addressCustomer);
+		if (createdDate!=null)
+			query.setParameter("createdDate", createdDate);
+		return query.getResultList();			
 		
 	}
 }
